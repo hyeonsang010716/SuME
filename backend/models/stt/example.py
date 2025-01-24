20716b8ee27f3cffdf6c8958e02db677937a06e2
sentryc0201 <yc020102@gmail.com>
2025-01-25 02:23:39 +0900

    add functions and update

--- backend/models/stt/example.py
+++ backend/models/stt/example.py
@@ -1,22 +1,27 @@
 import os
 from google.cloud import speech
 from google.cloud import storage
+from dotenv import load_dotenv
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:/Users/sentr/Downloads/sume-stt-7dc74e154409.json"
+# .env 파일 로드
+load_dotenv()
 
-def upload_blob(bucket_name, source_file_path, destination_blob_name):
+# 구글 서비스 계정 키 환경변수 지정정
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv('GOOGLE_APPLICATION_CREDENTIALS') 
+
+def upload_blob(bucket_name, source_file_path, file_name):
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
+    blob = bucket.blob(file_name)
     
     # 파일이 이미 존재하는지 확인하고, 존재하면 함수 종료
     if blob.exists():
-        print(f"File {destination_blob_name}가 이미 존재합니다.")
+        print(f"File {file_name}가 이미 존재합니다.")
         return
 
     blob.upload_from_filename(source_file_path)
-    print(f"{source_file_path}의 파일이 {bucket_name}에 {destination_blob_name}으로 업로드되었습니다.")
-    
+    print(f"{source_file_path}의 파일이 {bucket_name}에 {file_name}으로 업로드되었습니다.")
+
 def transcribe_audio_file(gcs_uri: str) -> str:
     
     client = speech.SpeechClient()
@@ -25,7 +30,7 @@ def transcribe_audio_file(gcs_uri: str) -> str:
     config = speech.RecognitionConfig(
         encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,  # .wav파일
         sample_rate_hertz=48000, # 샘플 레이트 48kHz(해당 파일의 샘플 레이트트)
-        language_code="ko-KR",  # 한글설정
+        language_code="ko-KR",  # 영어와 한글글설정
     )
 
     operation = client.long_running_recognize(config=config, audio=audio)
@@ -41,17 +46,40 @@ def transcribe_audio_file(gcs_uri: str) -> str:
 
     return transcript
 
-if __name__=="__main__":
-    # GCS 버킷 이름, 로컬 파일 경로, GCS에 업로드할 파일 이름 설정
+def delete_blob(bucket_name, file_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    
+    # 파일이 버킷에 이미 존재하는지 확인하고, 존재하지 않으면 함수 종료
+    if not blob.exists():
+        print(f"File {file_name}이 존재하지 않습니다.")
+        return
+
+    blob.delete()
+    print(f"File {file_name}가 삭제됨.")
+
+def process_all(file_name):
+    # 버킷 이름, 음성파일 경로 설정
     bucket_name = "sume_audio_bucket"
-    source_file_path = "C:/Users/sentr/Downloads/Trip (Feat. Hannah).wav"
-    destination_blob_name = "Trip (Feat. Hannah).wav"
-
-    # 파일 업로드
-    upload_blob(bucket_name, source_file_path, destination_blob_name)
-
-    # GCS URI 생성
-    gcs_uri = f"gs://{bucket_name}/{destination_blob_name}"
-
+    source_file_path = "./models/stt/example_audio_file/Trip (Feat. Hannah).wav"
+    
+    # 버킷에 음성파일 업로드
+    upload_blob(bucket_name, source_file_path, file_name)
+    
+    # GCS URI 생성 후 택스트로 변환
+    gcs_uri = f"gs://{bucket_name}/{file_name}"
     result_text = transcribe_audio_file(gcs_uri)
+    
+    # 버킷에 음성파일 삭제
+    delete_blob(bucket_name, file_name)
+    
+    return result_text
+    
+
+if __name__=="__main__":
+    # GCS에 업로드할 파일 이름 설정
+    file_name = "Trip (Feat. Hannah).wav"
+
+    result_text = process_all(file_name)
     print(result_text)
