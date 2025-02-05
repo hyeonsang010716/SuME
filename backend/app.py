20c9f4e97e1a87e0ad0229200616f8f46ac28f42
UH3135 <rabbitdy0101@kookmin.ac.kr>
2025-02-05 18:42:48 +0900

    REFACTOR: 환경변수 호출 코드 수정

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from api import create_app
 from models.stt.google_cloude import rInit_GoogleCloudeSTT
+from dotenv import load_dotenv
 
 
+load_dotenv()
 app = create_app()
 rInit_GoogleCloudeSTT()
 app.run(host="0.0.0.0", port=5000, debug=True)
